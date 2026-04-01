6e536d3b46e12dabbd66f61fbc6a68b033e43610
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-04-01 06:07:52 +0530

    Fix duplicate return statement in authMiddleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 const protect=async(req,res,next)=>{
     const token=req.headers.authorization;
     if(!token){
-        return res.status(401).json({message:"Unauthorized"})
+        return res.status(401).json({message:"Unauthorized"}) 
     }
     try{
         const decoded=jwt.verify(token,process.JWT_SECRET)
